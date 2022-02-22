c4e02cf813708cf90162d332e08fb698a9217313
mikkabiorange <100075452+mikkabiorange@users.noreply.github.com>
2022-02-22 15:17:09 +0900

    Update qwe.py

--- qwe.py
+++ qwe.py
@@ -39,48 +39,6 @@ Response.text
 # In[ ]:
 
 
-tomorrow=forecast.find_all("div")[1]
-
-
-# In[ ]:
-
-
-weather=tomorrow.find_all("p",attrs={"class":"pict"})[0].text.replace("\n","").replace(" ","")
-
-
-
-# In[ ]:
-
-
-rain_06=tomorrow.find_all("td")[4].text
-rain_612=tomorrow.find_all("td")[5].text
-rain_1218=tomorrow.find_all("td")[6].text
-rain_1824=tomorrow.find_all("td")[7].text
-
-
-# In[ ]:
-
-
-message="""
-明日の天気は{}
-最高気温は{}
-最低気温は{}
-0-6時:{}
-6-12時:{}
-12-18時:{}
-18-24時:{}
-""".format(weather,high,low,rain_06,rain_612,rain_1218,rain_1824)
-
-# In[ ]:
-
-
-auth={"Authorization":"Bearer "+token}
-content={"message":message}
-
-
-# In[ ]:
-
-
 requests.post(url, headers=auth, data=content)
 
 # %%
